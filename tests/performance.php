772baae362e9d4ad9731fb8bd84473c9f182eb5d
Sergey Viskov <voksiv@gmail.com>
2016-03-16 14:10:50 +0300

    few clean code

---
+++ tests/performance.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * TODO оберунть в PhpUnit
+ * @author: Viskov Sergey
+ * @date: 3/16/16
+ * @time: 2:04 PM
+ */
+
+use LTDBeget\sphinxConfigurator\serializers\PlainSerializer;
+
+require(__DIR__ . '/../vendor/autoload.php');
+
+$config_path = __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."stubs".DIRECTORY_SEPARATOR."valid.example.conf";
+$plain_config = file_get_contents($config_path);
+
+$times = [];
+
+$n = 1000;
+for($i = 1; $i <= $n; $i++) {
+    $start = microtime(true); //начало измерения
+
+    $config = PlainSerializer::deserialize($plain_config);
+
+    $end = microtime(true); //конец измерения
+
+    $times[] = $end - $start;
+}
+
+
+$average = array_sum($times)/count($times);
+$max = max($times);
+$min = min($times);
+echo "Среднее время выполнения ".$average.PHP_EOL;
+echo "Минимальное время выполнения ".$min.PHP_EOL;
+echo "Максимальное время выполнения ".$max.PHP_EOL;
+echo "Количество запусков ".$n.PHP_EOL;
